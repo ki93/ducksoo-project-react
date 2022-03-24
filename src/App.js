cca0454fcc0299fc11fa5758b8faa7d4faa822a1
vagrant <vagrant@ubuntu2004>
2022-03-24 05:47:43 +0000

    chore : change proxy, add npm package

--- src/App.js
+++ src/App.js
@@ -4,25 +4,10 @@ import React from 'react'
 import Router from 'Routes'
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 function App() {
   return (
     <div className="App">
-      {/* <Login></Login> */}
       <Router></Router>
-      {/* <Header></Header>
-      <Container fluid className="ContentArea">
-        <Row>
-          <Col sm={1} className="sideMenu">
-            <SideBar></SideBar>
-          </Col>
-          <Col sm={11}>
-            <Router></Router>
-          </Col>
-        </Row>
-      </Container>
-      <Footer></Footer> */}
-
     </div>
   );
 }
